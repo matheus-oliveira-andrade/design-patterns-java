c4d369becdcd728ad5e915b2693bcefb77321b60
Matheus de Oliveira <mathnhais@outlook.com>
2020-05-17 23:52:31 -0300

    Correção pattern adapter, adicionando vantagens e desvantagens

--- src/com/estrutural/adapter/ExecucaoAdapter.java
+++ src/com/estrutural/adapter/ExecucaoAdapter.java
@@ -19,6 +19,16 @@ public class ExecucaoAdapter {
 		 * Problema: Um sistema legado possui ao realizar uma transação uma estrutura de log, desejasse 
 		 * atualizar está estrutura sem alterar o sistema especifico		 
 		 * 
+		 * Vantagens:
+		 * 
+		 * - SRP ou principio da responsabilidade única, o padrão separa a lógica da transação da lógica primaria
+		 * - OCP ou principio do aberto/fechado, uma classe deve estar aberta para extensão e fechada para modificação. 
+		 * é possível introduzir novos adaptadores sem quebrar o código já existente, requerendo apenas que classe implementado 
+		 * a interfaceLogger
+		 * 
+		 * Desvantagens:
+		 * 
+		 * - Código mais complexo, pois todo novo adaptador implementado é necessario introduzir um novo conjunto de interfaces e classes
 		 * */
 		
 		TransacaoService servicoPagamento = new TransacaoService(new Logger());
